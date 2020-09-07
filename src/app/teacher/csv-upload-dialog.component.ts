e0e5503ce2e0852f4681f5b729cbc7c11d327da8
rebeccapelaca <s256702@studenti.polito.it>
2020-09-07 19:52:15 +0200

    upload csv - add message

--- src/app/teacher/csv-upload-dialog.component.ts
+++ src/app/teacher/csv-upload-dialog.component.ts
@@ -7,26 +7,6 @@ import { CsvStudent } from '../csv-student.model';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { DialogData } from '../auth/login-dialog.component';
 
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
-
 @Component({
   selector: 'app-csv-upload-dialog',
   templateUrl: './csv-upload-dialog.component.html',
@@ -43,7 +23,7 @@ export class CsvUploadDialogComponent implements OnInit {
     this.dataSource.paginator = this.paginator;
   }
 
-  displayedColumns: string[] = ['id', 'name', 'surname', 'email', 'done'];
+  displayedColumns: string[] = ['id', 'name', 'surname', 'email', 'done', 'message'];
 
   constructor(@Inject(MAT_DIALOG_DATA) public data: CsvStudent[]) {
     console.log("Sono nel costruttore");
